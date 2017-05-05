87e9d7fd746a77563ec7319cc68ed945c88521d1
JULIAN ALBERTO MANRIQUE PUERTO <ja.manrique@sis.virtual.uniandes.edu.co>
2017-05-05 16:56:30 -0500

    imprimir arbol

--- src/experimento/MaquinaEstructura.java
+++ src/experimento/MaquinaEstructura.java
@@ -8,35 +8,35 @@ public class MaquinaEstructura {
 
 	// Interfaz de las operaciones
 	public interface Operaciones{
-		
+
 		public Nodo buscar(int idNodo);
-		
+
 		public boolean agregar(Nodo nuevoNodo);
-		
+
 	}
-	
+
 	// Clase generica nodo
 	public class Nodo{
 		private int id;
-		
+
 		public Nodo(int id){
 			this.id = id;
 		}
-		
+
 		public int getId(){
 			return id;
 		}
 	}
-	
-	
-	
+
+
+
 	// Clase que representa al nodo de un arbol binario
 	public class NodoArbolBinario extends Nodo{
 
 		private NodoArbolBinario derecha;
-		
+
 		private NodoArbolBinario izquierda;
-		
+
 		public NodoArbolBinario(int id) {
 			super(id);
 		}
@@ -56,30 +56,49 @@ public class MaquinaEstructura {
 		public void setIzquierda(NodoArbolBinario izquierda) {
 			this.izquierda = izquierda;
 		}
-		
+
 	}
-	
+
 	// Clase que representa a un arbol binario
 	public class ArbolBinario implements Operaciones{
 
 		private NodoArbolBinario raiz;
-		
+
 		public ArbolBinario(NodoArbolBinario nodoRaiz) {
 			raiz = nodoRaiz;
 		}
-		
-		
-		
+
+		public void imprimir()
+		{
+			String respuesta = "";
+
+			inorden(raiz, respuesta);
+
+			System.out.println(respuesta);
+		}
+
+		private void inorden(NodoArbolBinario x, String msg) {
+
+			if (x != null)
+			{
+				inorden(x.izquierda, msg);
+				
+				msg += "["+x.getId()+"]";
+
+				inorden(x.derecha, msg);
+			}
+		}
+
 		public Nodo buscar(int idNodo) {
-			 
+
 			return buscar (raiz, idNodo);
 		}
-		
+
 		private Nodo buscar(NodoArbolBinario x, int buscado)
 		{
 			if (x == null)
 				return null;
-			
+
 			if (buscado > x.getId())
 				return buscar(x.derecha, buscado);
 			else if (buscado < x.getId())
@@ -93,19 +112,20 @@ public class MaquinaEstructura {
 			
 			return agregar(raiz, nuevoNodo);
 		}
-		
+
 		private boolean agregar(NodoArbolBinario x, Nodo agregado)
 		{
-			
-			if (x.getId() < agregado.getId())
+			System.out.println(x.getId() + " //" + agregado.getId());
+			if (x.getId() > agregado.getId())
 				if (x.izquierda != null)
 					return agregar(x.izquierda, agregado);
 				else
 				{
+					System.out.println("Eureka");
 					x.izquierda = x;
 					return true;
 				}
-			else if (x.getId()> agregado.getId())
+			else if (x.getId() < agregado.getId())
 				if (x.derecha != null)
 					return agregar(x.derecha, agregado);
 				else
@@ -116,55 +136,64 @@ public class MaquinaEstructura {
 			else//Son iguales y por ende ya existen; no reemplazo y retorno false
 				return false;
 		}
-		
+
 	}
-	
+
 	// Clase que representa a un nodo de un grafo
 	public class NodoGrafo extends Nodo{
-		
+
 		private NodoGrafo fuente;
 		private List<NodoGrafo> listaAdyacencia;
-		
+
 		public NodoGrafo(int id, NodoGrafo nodoFuente) {
 			super(id);
 			fuente = nodoFuente;
 			listaAdyacencia = new ArrayList<>();
 		}
-		
+
 		public List<NodoGrafo> getListaAdyacencia(){
 			return listaAdyacencia;
 		}
-		
+
 	}
-	
+
 	// Clase que representa a un grafo dirigido
 	public class GrafoDirigido implements Operaciones{
 
 		private NodoGrafo fuente;
-		
+
 		public GrafoDirigido(NodoGrafo nodoFuente) {
 			fuente = nodoFuente;
 		}
-		
+
 		@Override
 		public Nodo buscar(int idNodo) {
-			
+
 			return null;
 		}
 
 		@Override
 		public boolean agregar(Nodo nuevoNodo) {
-			
+
 			return false;
 		}
-		
+
 	}
-	
+
 	// Puede probar el funcionamiento de las operaciones en el main
 	public static void main(String args[]){
+
+		MaquinaEstructura x = new MaquinaEstructura();
 		
+		ArbolBinario arbol = x.new ArbolBinario(x.new NodoArbolBinario(2));
+		
+		arbol.agregar(x.new Nodo(2));
+		arbol.agregar(x.new Nodo(3));
+		arbol.agregar(x.new Nodo(4));
+		
+		arbol.imprimir();
 	}
-	
+
 	public class NodoLista extends Nodo
 	{
 		public NodoLista(int id) {
@@ -173,28 +202,48 @@ public class MaquinaEstructura {
 		private NodoLista siguiente;
 		private int valor;
 	}
-	
+
 	public class ListaEncadenada implements Operaciones
 	{
 
+		private NodoLista inicio;
+
 		@Override
 		public Nodo buscar(int idNodo) {
-			
-			N
-			
-			while
-			
+
+			NodoLista actual = inicio; 
+
+
+			while (actual != null)
+			{
+				if (actual.getId() == idNodo)
+					return actual;
+				actual = actual.siguiente;
+			}
+
 			return null;
 		}
 
 		@Override
 		public boolean agregar(Nodo nuevoNodo) {
-			// TODO Auto-generated method stub
-			return false;
+
+			NodoLista actual = inicio;
+
+			while (actual.siguiente != null)
+			{
+				if (actual.getId() == nuevoNodo.getId())
+					return false;
+				actual = actual.siguiente;
+			}
+
+			//actual.siguiente es null, lo cambio por el unevo
+			actual.siguiente = (NodoLista) nuevoNodo;
+
+			return true;
 		}
 
-		
-		
+
+
 	}
-	
+
 }
